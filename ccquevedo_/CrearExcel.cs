6bbf20a083ae9af3bc5e5a6ac725ddde53e1bd63
dennissezambrano2017 <66701370+dennissezambrano2017@users.noreply.github.com>
2022-05-06 15:52:34 -0500

    try-catch

--- ccquevedo_/CrearExcel.cs
+++ ccquevedo_/CrearExcel.cs
@@ -120,91 +120,64 @@ namespace ccquevedo_
 
         private void btnElimnar_Click(object sender, EventArgs e)
         {
-           
-            if (dgvProducto.SelectedRows.Count > 0)
+            try
             {
-                DialogResult result = MessageBox.Show("Esta seguro de eliminar la fila", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (result == DialogResult.Yes)
+
+                if (dgvProducto.SelectedRows.Count > 0)
                 {
-                    dgvProducto.Rows.Remove(dgvProducto.CurrentRow);
-                    dgvProducto.Rows[0].Selected = false;
+                    DialogResult result = MessageBox.Show("Esta seguro de eliminar la fila", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        dgvProducto.Rows.Remove(dgvProducto.CurrentRow);
+                        dgvProducto.Rows[0].Selected = false;
+                    }
+
                 }
-               
+                else
+                    MessageBox.Show("Seleccione una fila con datos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
             }
-            else
-                MessageBox.Show("Seleccione una fila con datos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void btnActu_Desca_Click(object sender, EventArgs e)
         {
-            if (dgvProducto.Rows != null && dgvProducto.Rows.Count != 0)
-            {
-                Insertar();
-                Exportar(dgvProducto);
-            }
-            else
-                MessageBox.Show("No existen datos para exportar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-        }
-
-        public void Exportar(DataGridView dtlist)
-        {
             try
             {
-                SaveFileDialog fichero = new SaveFileDialog();
-                fichero.Filter = "Excel (*.xls)|*.xls";
-                fichero.FileName = "Product-export-" + DateTime.Now.ToString("MM-dd-yy-H-mm-ss");
-                if (fichero.ShowDialog() == DialogResult.OK)
+                if (dgvProducto.Rows != null && dgvProducto.Rows.Count != 0)
                 {
-                    Microsoft.Office.Interop.Excel.Application exportarExcel;
-                    Microsoft.Office.Interop.Excel.Workbook libros_trabajo;
-                    Microsoft.Office.Interop.Excel.Worksheet hoja_trabajo;
-
-                    exportarExcel = new Microsoft.Office.Interop.Excel.Application();
-                    libros_trabajo = exportarExcel.Workbooks.Add();
-                    hoja_trabajo = 
-                        (Microsoft.Office.Interop.Excel.Worksheet)libros_trabajo.Worksheets.get_Item(1);
-
-                    int indiecolumna = 0;
-                    foreach (DataGridViewColumn columna in dtlist.Columns)
-                    {
-                        indiecolumna++;
-                        exportarExcel.Cells[1, indiecolumna] = columna.Name;
-                    }
-
-                    int indicefila = 0;
-                    foreach (DataGridViewRow fila in dtlist.Rows)
-                    {
-                        indicefila++;
-                        indiecolumna = 0;
-                        foreach (DataGridViewColumn columna in dtlist.Columns)
-                        {
-                            indiecolumna++;
-                            exportarExcel.Cells[indicefila + 1, indiecolumna] = fila.Cells[columna.Name].Value;
-                        }
-                    }
-
-                    libros_trabajo.SaveAs(fichero.FileName, 
-                        Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal);
-                    libros_trabajo.Close(true);
-                    exportarExcel.Quit();
+                    Insertar();
+                    Exportar exportar = new Exportar();
+                    exportar.ExportarDatos(dgvProducto);
                 }
+                else
+                    MessageBox.Show("No existen datos para exportar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error al exportar la informacion debido a: " + ex.ToString());
+                MessageBox.Show(ex.ToString());
             }
-}
+        }
 
         private void btnActualizar_Click(object sender, EventArgs e)
         {
-            
-            if(dgvProducto.Rows.Count > 0)
+            try
             {
-                Insertar();
+                if (dgvProducto.Rows.Count > 0)
+                {
+                    Insertar();
+                }
+                else
+                    MessageBox.Show("No existen datos en la tabla para actualizar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-            else
-                MessageBox.Show("No existen datos en la tabla para actualizar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void Insertar()
